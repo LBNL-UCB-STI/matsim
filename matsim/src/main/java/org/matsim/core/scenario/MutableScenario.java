ee5b981dc7084db63818590f9d78e9dcdab2c318
Carlos Caldas <ccaldas@gmail.com>
2019-06-07 19:57:36 +0100

    separated line

--- matsim/src/main/java/org/matsim/core/scenario/MutableScenario.java
+++ matsim/src/main/java/org/matsim/core/scenario/MutableScenario.java
@@ -213,6 +213,7 @@ public final class MutableScenario implements Scenario, Lockable {
 		testForLocked();
 		this.transitVehicles = vehicles ;
 	}
+
 	public final void setLanes( Lanes lanes ) {
 		testForLocked();
 		this.lanes = lanes ;
